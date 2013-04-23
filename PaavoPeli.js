e55fccb6734ad9f2b5b97d06b3bc4db17c676954
Koivisto <pelto.karvanen@gmail.com>
2013-04-23 14:30:55 +0300

    Update PaavoPeli.js

--- PaavoPeli.js
+++ PaavoPeli.js
@@ -12,27 +12,59 @@ function Village(x, y, topic, name){
 	this.topic=topic;
 	this.name=name;
 }
+Village.prototype.giveTopic(){
+	return this.topic;
+}
+Village.prototype.addTopic(topic){
+	this.topic = topic;
+}
 
-var Saarijarvi= new Village (100, 100, undefined, "Saarijärvi");
-var Autio= new Village (150, 150, undefined, "Autio");
 
-function Officer(ability){
+var Saarijarvi= new Village (100, 100, null, "Saarijärvi");
+var Autio= new Village (150, 150, null, "Autio");
+
+var Villages = [Saarijarvi, Autio];//en muista toimiko js listat näin
+
+
+function Officer(theme){
 	this.chosen=false;//by default not chosen
 		//can be Village or Route
 	this.position=Saarijarvi;//by def in Saarijärvi
 		//village or null, if null no movement
 	this.target=null;
-	this.ability=ability;
+	this.ability=theme;
 }
 
-function Topic(){
-
+function Topic(theme){
+	this.theme = theme;//same as officers ability
 }
 
 function TopicGenerator(){
-	this.activeTopics = 6;
+	this.maxTopics = 4;//4 by default
+	this.activeTopics = 0;//no topics by default
 	this.topicWaitTime = 10000; //ms = 10 sec
 	this.topicHandlingTime = 1000; //ms = 1 sec
+	
+	if(maxTopics > Villages.lenght){ //if too manu topics...
+		this.maxTopics = Villages.lenght;
+	}
+}
+TopicGenerator.prototype.updateTopics(){
+	if (activeTopics < maxTopics){
+		//random LISÄÄ
+		var topicInVillage = Villages[1].giveTopic;//miten taulukon jäseneen
+		if(topicInVillge == null){
+			var topicToVillage = new Topic(0);//enumeraatio???? LISÄÄ JOKU
+			Villages[1].addTopic(topicToVillage); //lisää suodun topicin kylälle
+		}
+	}
+
+}
+
+TopicGenerator.prototype.addChallenge(){
+	if(maxTopics < Villages.lenght){//if its possible to add
+		this.maxTopics ++;
+	}
 }
 
 function Route(village1, village2){
