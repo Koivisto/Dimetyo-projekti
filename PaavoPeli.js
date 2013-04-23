36bff0c48969687b18d9032e003afc845f233bfe
Koivisto <pelto.karvanen@gmail.com>
2013-04-23 14:06:37 +0300

    Update PaavoPeli.js
    lisaa

--- PaavoPeli.js
+++ PaavoPeli.js
@@ -1,15 +1,50 @@
-/*  PaavoPeli
-		Tehty studio2 ja dimetyö kurssien projektina
+/*	PaavoPeli
+	Author: Aarne Leinonen
+	Tehty studio2 ja dimetyö kurssien projektina
 */
 
 var peliVoitettu = false;
 
-function Village(
 
-Officer
+function Village(x, y, topic, name){
+	this.x=x;
+	this.y=y;
+	this.topic=topic;
+	this.name=name;
+}
 
-Topic
+var Saarijarvi= new Village (100, 100, undefined, "Saarijärvi");
+var Autio= new Village (150, 150, undefined, "Autio");
 
-TopicGenerator
+function Officer(ability){
+	this.chosen=false;//by default not chosen
+		//can be Village or Route
+	this.position=Saarijarvi;//by def in Saarijärvi
+		//village or null, if null no movement
+	this.target=null;
+	this.ability=ability;
+}
 
-Route
+function Topic(){
+
+}
+
+function TopicGenerator(){
+	this.activeTopics = 6;
+	this.topicWaitTime = 10000; //ms = 10 sec
+	this.topicHandlingTime = 1000; //ms = 1 sec
+}
+
+function Route(village1, village2){
+	this.village1=village1;
+	this.village2=village2;
+}
+
+/*
+				Autio------<
+Saarijärvi----<
+				Kyyjärvi----<
+*/
+var SaarijarviAutio = new Route (Saarijarvi, Autio);
+
+
